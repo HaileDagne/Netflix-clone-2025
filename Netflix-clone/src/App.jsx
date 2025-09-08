a3a5cb849261d5cd7f192698d18057fc1302a3f6
Haile Dagne <haile.dange0@gmail.com>
2025-09-08 08:55:05 -0400

    Header and Footer

--- Netflix-clone/src/App.jsx
+++ Netflix-clone/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-import './App.css'
+import Home from './Pages/Home/Home'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App =() => {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
+      <Footer />
     </>
   )
 }
---
+++ Netflix-clone/src/components/Footer/Footer.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import './Footer.css'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import CopyrightIcon from '@mui/icons-material/Copyright';
+
+function Footer() {
+  return (
+    <>
+      <div className="footer-outer-container">
+        <div className="footer-inner-container">
+          <div className="footer-icons">
+            <ul>
+              <li>
+                <FacebookOutlinedIcon />
+              </li>
+              <li>
+                <InstagramIcon />
+              </li>
+              <li>
+                <YouTubeIcon />
+              </li>
+            </ul>
+          </div>
+
+          <div className="footer-data">
+            <div>
+              <ul>
+                <li>Audio Description</li>
+                <li>Investor relations</li>
+                <li>Legal Notices</li>
+              </ul>
+            </div>
+
+            <div>
+              <ul>
+                <li>Help Center</li>
+                <li>Jobs</li>
+                <li> Preferences</li>
+              </ul>
+            </div>
+
+            <div>
+              <ul>
+                <li>Gift Cards </li>
+                <li> Terms of Use</li>
+                <li> Information</li>
+              </ul>
+            </div>
+
+            <div>
+              <ul>
+                <li>Media Center</li>
+                <li>Privacy</li>
+                <li>Contact Us</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="service-code">
+            <p>Service Code</p>
+          </div>
+
+          <div className="copy-right"> &copy: 1997 - 2025 Netflix, Inc</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
